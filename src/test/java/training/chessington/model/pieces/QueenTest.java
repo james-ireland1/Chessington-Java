a4472604ca99d736efd33b67dd5a89c14a44bb27
James Ireland <james.ireland@softwire.com>
2025-06-23 10:55:48 +0100

    add queen tests (only a couple written to save time)

--- src/test/java/training/chessington/model/pieces/QueenTest.java
+++ src/test/java/training/chessington/model/pieces/QueenTest.java
@@ -1,7 +1,45 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class QueenTest {
+    @Test
+    public void whiteQueenCanMoveUpRightOneSquare() {
+        // Arrange
+        Board board = Board.empty();
+        Piece queen = new Queen(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7, 3);
+        board.placePiece(coords, queen);
+
+        // Act
+        List<Move> moves = queen.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 1)));
+    }
+
+    @Test
+    public void whiteQueenCanMoveDownLeftOneSquare() {
+        // Arrange
+        Board board = Board.empty();
+        Piece queen = new Queen(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(0, 3);
+        board.placePiece(coords, queen);
+
+        // Act
+        List<Move> moves = queen.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -1)));
+    }
 }
