015bd59d655e93fee7a65504856378c40ab5b340
James Ireland <james.ireland@softwire.com>
2025-06-23 11:51:08 +0100

    added more knight tests which all pass

--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -36,5 +36,55 @@ public class KnightTest {
         assertThat(moves).contains(new Move(coords, coords.plus(2,1)));
     }
 
+    @Test
+    public void whiteKnightInCornerOnlyHasTwoMoves() {
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(0,0);
+        board.placePiece(coords, knight);
 
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        assertThat(moves).contains(new Move(coords, coords.plus(1,2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2,1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(2, -1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, -2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, -2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, 2)));
+    }
+
+    @Test
+    public void blackKnightCanCaptureWhitePawn() {
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(0,1);
+        board.placePiece(coords, knight);
+
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates pawnCoords = new Coordinates(2,2);
+        board.placePiece(pawnCoords, pawn);
+
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        assertThat(moves).contains(new Move(coords, coords.plus(2,1)));
+    }
+
+    @Test
+    public void whiteKnightCannotCaptureWhitePawn() {
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7,6);
+        board.placePiece(coords, knight);
+
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates pawnCoords = new Coordinates(6,4);
+        board.placePiece(pawnCoords, pawn);
+
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, -2)));
+    }
 }
