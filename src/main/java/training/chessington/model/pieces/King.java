6079191140aa91fc4921ab900a37d4bf0c8fb70b
James Ireland <james.ireland@softwire.com>
2025-06-23 12:11:16 +0100

    King passes basic tests

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -15,6 +15,27 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> output = new ArrayList<>();
+
+        Coordinates up = new Coordinates(-1,0);
+        Coordinates upRight = new Coordinates(-1,1);
+        Coordinates right = new Coordinates(0,1);
+        Coordinates downRight = new Coordinates(1,1);
+        Coordinates down = new Coordinates(1,0);
+        Coordinates downLeft = new Coordinates(1,-1);
+        Coordinates left = new Coordinates(0,-1);
+        Coordinates upLeft = new Coordinates(-1,-1);
+
+        output.addAll(this.getAllMovesInOneDirection(from, board, up, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, upRight, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, right, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, downRight, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, down, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, downLeft, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, left, 1));
+        output.addAll(this.getAllMovesInOneDirection(from, board, upLeft, 1));
+
+
+        return output;
     }
 }
