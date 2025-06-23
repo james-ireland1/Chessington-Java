97095b3bc1786d3de645982f8d0dc648d8c8080f
James Ireland <james.ireland@softwire.com>
2025-06-23 11:16:21 +0100

    knight can move in 8 directions

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -15,6 +15,26 @@ public class Knight extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> output = new ArrayList<>();
+
+        Coordinates upUpRight = new Coordinates(-2,1);
+        Coordinates upRightRight = new Coordinates(-1,2);
+        Coordinates downRightRight = new Coordinates(1,2);
+        Coordinates downDownRight = new Coordinates(2,1);
+        Coordinates downDownLeft = new Coordinates(2,-1);
+        Coordinates downLeftLeft = new Coordinates(1,-2);
+        Coordinates upLeftLeft = new Coordinates(-1,-2);
+        Coordinates upUpLeft = new Coordinates(-2,-1);
+
+        output.add(new Move(from, from.plus(upUpRight.getRow(), upUpRight.getCol())));
+        output.add(new Move(from, from.plus(upRightRight.getRow(), upRightRight.getCol())));
+        output.add(new Move(from, from.plus(downRightRight.getRow(), downRightRight.getCol())));
+        output.add(new Move(from, from.plus(downDownRight.getRow(), downDownRight.getCol())));
+        output.add(new Move(from, from.plus(downDownLeft.getRow(), downDownLeft.getCol())));
+        output.add(new Move(from, from.plus(downLeftLeft.getRow(), downLeftLeft.getCol())));
+        output.add(new Move(from, from.plus(upLeftLeft.getRow(), upLeftLeft.getCol())));
+        output.add(new Move(from, from.plus(upUpLeft.getRow(), upUpLeft.getCol())));
+
+        return output;
     }
 }
